04a01881f1401b20164fb8666dc601bf6664faff
Adam Kuszczak <kuszczak.adam@gmail.com>
2017-02-12 20:53:16 +0000

    Some publishing tasks and clean up

--- mintimedialog/src/main/java/eu/tesseractsoft/mintimedialog/MinTimeDialog.java
+++ mintimedialog/src/main/java/eu/tesseractsoft/mintimedialog/MinTimeDialog.java
@@ -5,6 +5,10 @@ import android.content.Context;
 import android.os.Handler;
 
 /**
+ * Progress Dialog with Minimum Showing Time. It assures that dialog will be shown for
+ * at least specified amount of time. It also offers silent dismiss, without firing dismiss listener
+ *
+ *
  * Created by Adam Kuszczak on 11/02/2017.
  */
 
@@ -20,17 +24,28 @@ public class MinTimeDialog extends ProgressDialog {
     private boolean mMinShowingTimeAchieved;
 
 
+    /**
+     * Simple constructor
+     *
+     * @param context context
+     */
     public MinTimeDialog(Context context) {
         super(context);
         init();
     }
 
+    /**
+     * Constructor with theme
+     *
+     * @param context context
+     * @param theme   theme resource
+     */
     public MinTimeDialog(Context context, int theme) {
         super(context, theme);
         init();
     }
 
-    private void init(){
+    private void init() {
         mMinShownTimeMs = 0;
         mSilentDismiss = false;
 
@@ -39,38 +54,60 @@ public class MinTimeDialog extends ProgressDialog {
         mMinShowingTimeAchieved = false;
     }
 
+    /**
+     * Setter for specifying minimum showing time
+     *
+     * @param minShownTimeMs minimum showing time in milliseconds, default 0
+     */
     public void setMinShownTimeMs(int minShownTimeMs) {
         this.mMinShownTimeMs = minShownTimeMs;
     }
 
+    /**
+     * Option to enable silent dismiss
+     *
+     * @param silentDismiss true if silent dismiss should be enabled, default false
+     */
     public void setSilentDismiss(boolean silentDismiss) {
         this.mSilentDismiss = silentDismiss;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void show() {
-        if(mMinShownTimeMs > 0) {
+        if (mMinShownTimeMs > 0) {
             mHandler.postDelayed(minShowingTimeTimeout, mMinShownTimeMs);
         }
         super.show();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public synchronized void dismiss() {
         mDismissAlreadyRequested = true;
-        if(mMinShowingTimeAchieved) {
+        if (mMinShowingTimeAchieved) {
             super.dismiss();
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setOnDismissListener(OnDismissListener listener) {
-        if(!mSilentDismiss) {
+        if (!mSilentDismiss) {
             super.setOnDismissListener(listener);
         }
     }
 
-    public synchronized void dismissForced(){
+    /**
+     * Immediately dismiss the dialog ignoring minimum showing time
+     */
+    public synchronized void dismissForced() {
         mHandler.removeCallbacks(minShowingTimeTimeout);
         super.dismiss();
     }
@@ -79,7 +116,7 @@ public class MinTimeDialog extends ProgressDialog {
         @Override
         public void run() {
             mMinShowingTimeAchieved = true;
-            if(mDismissAlreadyRequested){
+            if (mDismissAlreadyRequested) {
                 dismissForced();
             }
         }
@@ -88,7 +125,15 @@ public class MinTimeDialog extends ProgressDialog {
 
     // =========== Static builder methods ===============
 
-    public static MinTimeDialog createMinTimeDialog(Context context, String message, int minShownTimeMs){
+    /**
+     * Simple builder method to create dialog with just message and minimum showing time
+     *
+     * @param context        context
+     * @param message        message inside the standard dialog
+     * @param minShownTimeMs minimum showing time
+     * @return instance of MinTimeDialog
+     */
+    public static MinTimeDialog createMinTimeDialog(Context context, String message, int minShownTimeMs) {
         MinTimeDialog dialog = new MinTimeDialog(context);
         dialog.setMessage(message);
         dialog.setMinShownTimeMs(minShownTimeMs);
